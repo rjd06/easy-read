4267c547df1380dff2153f946acab6066b7706f8
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-27 22:11:38 +0530

    getToken() utils added.

--- src/utils/token.js
+++ src/utils/token.js
@@ -2,4 +2,9 @@ import jwt from "jsonwebtoken";
 
 export const signToken = (id)=>{
     return jwt.token({id}, process.env.JWT_SECRET, {expiresIn: "7d"});
-}
+};
+
+export const getToken = (req)=>{
+    const h = req.headers.authorization || "";
+    return h.startsWith("Bearer ")?h.slice(7) : null;
+};
