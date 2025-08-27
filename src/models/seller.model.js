17ec73703d051e37f39ac6183af5a1431d043ccd
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-27 22:29:59 +0530

    password hashed using bcrypt

--- src/models/seller.model.js
+++ src/models/seller.model.js
@@ -28,5 +28,13 @@ const sellerSchema = new mongoose.Schema({
 }, {timestamps:true});
 
 
+sellerSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = bcrypt.hash(this.password, salt);
+    next();
+})
+
 
 export default Seller = mongoose.model("Seller", sellerSchema);
