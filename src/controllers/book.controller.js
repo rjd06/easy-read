3d700782c64032e4adb0b022b85e85313d33aed1
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-09-02 20:02:54 +0530

    getBookById() added.

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -8,3 +8,14 @@ export const getAllBooks = async(req , res )=>{
         res.status(500).json({message:"Server Error"});
     }
 };
+
+export const getBookById = async(req, res)=>{
+    try {
+        const book = await Book.findById(req.params.id).populate("seller", "name storeName");
+
+        if(!book) return res.status(404).json({message: "Book Not Found"});
+        res.json(book);
+    } catch (error) {
+        res.status(500).json({message:"Server Error"});
+    }
+}
