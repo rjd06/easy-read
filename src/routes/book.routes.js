93bfc0b2b1e3fafb33a4c0e3defd6d69a2e95722
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-09-02 20:37:36 +0530

    book routing updated.

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { sellerProtect } from "../middleware/authMiddleware.js";
+import { createBook, deleteBook, getAllBooks, getBookById, updateBook } from "../controllers/book.controller.js";
 
 const router = Router();
-router.get("/", );
-router.get("/:id",);
-router.post("/", sellerProtect,);
-router.put("/:id", sellerProtect,);
-router.delete("/:id", sellerProtect,);
+router.get("/", getAllBooks );
+router.get("/:id", getBookById);
+router.post("/", sellerProtect, createBook);
+router.put("/:id", sellerProtect,updateBook);
+router.delete("/:id", sellerProtect,deleteBook);
 
 export default router;
